69223c0aa4a6bbbf2c02d1f7f30d998169ae32eb
rahimids <zahra.rahimi.ds@gmail.com>
2026-06-22 22:38:02 +0330

    Create for_loops.py

---
+++ for_loops.py
@@ -0,0 +1,18 @@
+# Exercise 1: Print numbers from 1 to 10
+
+for i in range(1, 11):
+    print(i)
+
+print("----------")
+
+# Exercise 2: Print even numbers from 2 to 20
+
+for i in range(2, 21, 2):
+    print(i)
+
+print("----------")
+
+# Exercise 3: Countdown from 10 to 1
+
+for i in range(10, 0, -1):
+    print(i)
